267cc45b9f451e8ff863107eec72522dcd15a66d
Sydney Tolbert <sydneykaturah@Sydneys-MacBook-Pro.local>
2018-09-16 17:56:59 -0400

    <getting there>

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,70 +1,70 @@
 $(document).ready(function() {
-    var wins = 0
-    var losses = 0
-    var goalScore = 0
-    var userScore = 0
-    var yellowValue = 0
-    var blueValue = 0
-    var emeraldValue = 0
-    var purpleValue = 0
-    var crystalValue = 0
+    var wins = 0;
+    var losses = 0;
+    var goalScore = 0;
+    var userScore = 0;
+    var yellowValue = 0;
+    var blueValue = 0;
+    var emeraldValue = 0;
+    var purpleValue = 0;
+    var crystalValue = 0;
+    var newScore = 0;
 
     $('#newGameButton').on('click', () => {
-        var userScore = 0;
-        console.log("clicked");
-        var goalScore = Math.floor(Math.random()*10);
+        userScore = 0;
+        newScore = 0;
+        goalScore = Math.floor(Math.random()*10);
         console.log(goalScore);
-        var yellowValue = Math.floor(Math.random()*10);
+        yellowValue = Math.floor(Math.random()*10);
         console.log(yellowValue);
-        var blueValue = Math.floor(Math.random()*10);
+        blueValue = Math.floor(Math.random()*10);
         console.log(blueValue);
-        var emeraldValue = Math.floor(Math.random()*10);
+        emeraldValue = Math.floor(Math.random()*10);
         console.log(emeraldValue);
-        var purpleValue = Math.floor(Math.random()*10);
+        purpleValue = Math.floor(Math.random()*10);
         console.log(purpleValue);
         $("#goalScoreNumber").text(goalScore);
-        $("#userScoreNumber").text(0);
+        $("#userScoreNumber").text(userScore);
         //display new goal score
         //clear user score
         //update win/loss counter?
 
         $('#yellow').on('click', function() {
             crystalValue = yellowValue;
-            gameCounter();
+            gameCounter(userScore, goalScore, crystalValue);
         });
 
         $('#blue').on('click', function(){
             crystalValue = blueValue;
-            gameCounter();
+            gameCounter(userScore, goalScore, crystalValue);
         });
 
         $('#emerald').on('click', function() {
             crystalValue = emeraldValue;
-            gameCounter();
+            gameCounter(userScore, goalScore, crystalValue);
         });
     
         $('#purple').on('click', function(){
             crystalValue = emeraldValue;
-            gameCounter();
+            gameCounter(userScore, goalScore, crystalValue);
         });
     });
 
-    function gameCounter() {
-        while (userScore <= goalScore) {
-            userScore = crystalValue + userScore
-            if (userScore < goalScore) {
-                $('#userScoreNumber').text(userScore);
+    function gameCounter(userNum, goalNum, crystalNum) {
+        newScore = crystalNum + userNum;
+            if (newScore < goalNum) {
+                $('#userScoreNumber').text(newScore);
+                userScore = newScore
             }
-            else if (userScore > goalScore) {
-                $('#userScore').text(userScore);
+            else if (newScore > goalNum) {
+                $('#userScoreNumber').text(newScore);
                 alert("You lost! Click New Game to try again.");
                 $("#lossesNumber").text(losses + 1);
             }
-            else if (userScore === goalScore) {
-                $('#userScore').text(userScore);
+            else if (newScore === goalNum) {
+                $('#userScoreNumber').text(newScore);
                 alert("You won!!! Click New Game to play again.");
                 $('#winsNumber').text(wins + 1);
             };
-        };
     };
 });

ee9e32198ddc4037fecc0bb605c859ffd3373882
Sydney Tolbert <sydneykaturah@Sydneys-MacBook-Pro.local>
2018-09-16 20:17:34 -0400

    <progress>

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,77 +2,73 @@ $(document).ready(function() {
     var wins = 0;
     var losses = 0;
     var userScore = 0;
-    var newScore = 0;
+    var goalScore = 0;
+    var yellowValue = 0;
+    var blueValue = 0;
+    var emeraldValue = 0;
+    var purpleValue = 0;
 
     $('#newGameButton').on('click', () => {
-        console.log("new game started");
+        console.log("NEW GAME STARTED");
         userScore = 0;
-        console.log("userScore: " + userScore);
         newScore = 0;
-        console.log("newScore: " + newScore);
-        var goalScore = Math.floor(Math.random()*10);
-        console.log("goalScore: " + goalScore);
-        var yellowValue = Math.floor(Math.random()*10);
-        console.log("yellowValue: " + yellowValue);
-        var blueValue = Math.floor(Math.random()*10);
-        console.log("blueValue: " + blueValue);
-        var emeraldValue = Math.floor(Math.random()*10);
-        console.log("emeraldValue: " + emeraldValue);
-        var purpleValue = Math.floor(Math.random()*10);
-        console.log("purpleValue: " + purpleValue);
-        $("#goalScoreNumber").text(goalScore);
         $("#userScoreNumber").text(userScore);
-        //display new goal score
-        //clear user score
-        //update win/loss counter?
+        //console.log("userScore: " + userScore);
+        //console.log("newScore: " + newScore);
+        goalScore = Math.floor(Math.random()*20);
+        //console.log("goalScore: " + goalScore);
+        yellowValue = Math.floor(Math.random()*10);
+        //console.log("yellowValue: " + yellowValue);
+        blueValue = Math.floor(Math.random()*10);
+        //console.log("blueValue: " + blueValue);
+        emeraldValue = Math.floor(Math.random()*10);
+        //console.log("emeraldValue: " + emeraldValue);
+        purpleValue = Math.floor(Math.random()*10);
+        //console.log("purpleValue: " + purpleValue);
+        $("#goalScoreNumber").text(goalScore);
 
         $('#yellow').on('click', function() {
-            crystalValue = yellowValue;
-            gameCounter(userScore, goalScore, crystalValue);
-            console.log("yellow selected");
+            userScore = userScore + yellowValue;
+            gameCounter(userScore, goalScore);
+            //console.log("yellow selected");
         });
 
         $('#blue').on('click', function() {
-            crystalValue = blueValue;
-            gameCounter(userScore, goalScore, crystalValue);
-            console.log("blue selected");
+            userScore = userScore + blueValue;
+            gameCounter(userScore, goalScore);
+            //console.log("blue selected");
         });
 
         $('#emerald').on('click', function() {
-            crystalValue = emeraldValue;
-            gameCounter(userScore, goalScore, crystalValue);
-            console.log("emerald selected");
+            userScore = userScore + emeraldValue;
+            gameCounter(userScore, goalScore);
+            //console.log("emerald selected");
         });
     
         $('#purple').on('click', function() {
-            crystalValue = purpleValue;
-            gameCounter(userScore, goalScore, crystalValue);
-            console.log("purple selected");
+            userScore = userScore + purpleValue;
+            gameCounter(userScore, goalScore);
+            //console.log("purple selected");
         });
     });
 
-    function gameCounter(userNum, goalNum, crystalNum) {
-        newScore = crystalNum + userNum;
-        if (newScore < goalNum) {
-            $('#userScoreNumber').text(newScore);
-            userScore = newScore;
+    function gameCounter(userNum, goalNum) {
+        if (userNum < goalNum) {
+            $('#userScoreNumber').text(userNum);
             console.log("keep going");
             console.log("userScore: " + userScore);
-            console.log("newScore: " + newScore);
         }
-        else if (newScore > goalNum) {
-            $('#userScoreNumber').text(newScore);
+        else if (userNum > goalNum) {
+            $('#userScoreNumber').text(userNum);
             alert("You lost! Click New Game to try again.");
             $("#lossesNumber").text(losses + 1);
             console.log("lose");
-            console.log(newScore);
         }
-        else if (newScore === goalNum) {
-            $('#userScoreNumber').text(newScore);
+        else if (userNum === goalNum) {
+            $('#userScoreNumber').text(userNum);
             alert("You won!!! Click New Game to play again.");
             $('#winsNumber').text(wins + 1);
             console.log("win");
-            console.log(newScore);
         };
     };
 });

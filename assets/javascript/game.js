c6729042815b6dd1895a0a5bf5bdd593dead422b
Sydney Tolbert <sydneykaturah@Sydneys-MacBook-Pro.local>
2018-09-16 20:36:23 -0400

    <final>

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,62 +13,47 @@ $(document).ready(function() {
         userScore = 0;
         newScore = 0;
         $("#userScoreNumber").text(userScore);
-        //console.log("userScore: " + userScore);
-        //console.log("newScore: " + newScore);
-        goalScore = Math.floor(Math.random()*20);
-        //console.log("goalScore: " + goalScore);
-        yellowValue = Math.floor(Math.random()*10);
-        //console.log("yellowValue: " + yellowValue);
-        blueValue = Math.floor(Math.random()*10);
-        //console.log("blueValue: " + blueValue);
-        emeraldValue = Math.floor(Math.random()*10);
-        //console.log("emeraldValue: " + emeraldValue);
-        purpleValue = Math.floor(Math.random()*10);
-        //console.log("purpleValue: " + purpleValue);
+        goalScore = Math.floor(Math.random()*111) + 19;
+        yellowValue = Math.floor(Math.random()*11) + 1;
+        blueValue = Math.floor(Math.random()*11) + 1;
+        emeraldValue = Math.floor(Math.random()*11) + 1;
+        purpleValue = Math.floor(Math.random()*11) + 1;
         $("#goalScoreNumber").text(goalScore);
+    });
 
-        $('#yellow').on('click', function() {
-            userScore = userScore + yellowValue;
-            gameCounter(userScore, goalScore);
-            //console.log("yellow selected");
-        });
+    $('#yellow').on('click', function() {
+        userScore = userScore + yellowValue;
+        gameCounter(userScore, goalScore);
+    });
 
-        $('#blue').on('click', function() {
-            userScore = userScore + blueValue;
-            gameCounter(userScore, goalScore);
-            //console.log("blue selected");
-        });
+    $('#blue').on('click', function() {
+        userScore = userScore + blueValue;
+        gameCounter(userScore, goalScore);
+    });
 
-        $('#emerald').on('click', function() {
-            userScore = userScore + emeraldValue;
-            gameCounter(userScore, goalScore);
-            //console.log("emerald selected");
-        });
+    $('#emerald').on('click', function() {
+        userScore = userScore + emeraldValue;
+        gameCounter(userScore, goalScore);
+    });
     
-        $('#purple').on('click', function() {
-            userScore = userScore + purpleValue;
-            gameCounter(userScore, goalScore);
-            //console.log("purple selected");
-        });
+    $('#purple').on('click', function() {
+        userScore = userScore + purpleValue;
+        gameCounter(userScore, goalScore);
     });
 
     function gameCounter(userNum, goalNum) {
         if (userNum < goalNum) {
             $('#userScoreNumber').text(userNum);
-            console.log("keep going");
-            console.log("userScore: " + userScore);
         }
         else if (userNum > goalNum) {
             $('#userScoreNumber').text(userNum);
             alert("You lost! Click New Game to try again.");
             $("#lossesNumber").text(losses + 1);
-            console.log("lose");
         }
         else if (userNum === goalNum) {
             $('#userScoreNumber').text(userNum);
             alert("You won!!! Click New Game to play again.");
             $('#winsNumber').text(wins + 1);
-            console.log("win");
         };
     };
 });
